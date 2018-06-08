fa826634e182d66c73fbaf44f6031cf8d5c87bc5
Anders Quist <anders.quist@stena.com>
2018-06-08 09:00:27 +0200

    A two more movies and a readme.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Trailer Website
+An application that renders a webpage containing the movies that are provided in the file `entertainment_center.py`.
+
+## Usage
+The application is started using the following command:
+```
+python entertainment_center.py
+```
+Which will in turn render a html page named `fresh_tomatoes.html` is the same directory and open it in your default webbrowser.
--- entertainment_center.py
+++ entertainment_center.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     toy_story = media.Movie("Toy Story", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                             "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-    avatar = media.Movie("Avatar", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+    avatar = media.Movie("Avatar", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                         "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+    die_hard = media.Movie("Die Hard", "https://upload.wikimedia.org/wikipedia/en/7/7e/Die_hard.jpg",
+                           "https://www.youtube.com/watch?v=2TQ-pOvI6Xo")
 
     # Make a call to render page and open web browser
-    fresh_tomatoes.open_movies_page([toy_story, avatar])
+    fresh_tomatoes.open_movies_page([toy_story, avatar, die_hard])
